b042c495eb9e6c5f98fc2ea6c85d55c4b355f496
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-06-15 13:19:31 +1200

    1.0.1 - 1.19.3-1.19.4 Backport

--- src/main/java/com/mclegoman/perspective/common/data/PerspectiveData.java
+++ src/main/java/com/mclegoman/perspective/common/data/PerspectiveData.java
@@ -22,7 +22,7 @@ public class PerspectiveData {
     public static final Integer MINOR_VERSION = 0;
     public static final Integer PATCH_VERSION = 1;
     public static final RTUReleaseTypes RELEASE_TYPE = RTUReleaseTypes.RELEASE;
-    public static final Integer BUILD_VERSION = 1;
+    public static final Integer BUILD_VERSION = 2;
     public static final Boolean IS_DEVELOPMENT = RTUReleaseTypeHelper.isDevelopment(RELEASE_TYPE);
     public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + PATCH_VERSION + "-" + RTUReleaseTypeHelper.getString(RELEASE_TYPE, RTUTranslationTypes.NORMAL) + "." + BUILD_VERSION;
     public static final String PREFIX = "[" + NAME + " " + VERSION + "] ";
