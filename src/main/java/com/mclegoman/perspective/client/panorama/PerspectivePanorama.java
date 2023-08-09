e2631213aaf919527c781a34865e05063f82340a
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-08-09 19:16:46 +1200

    1.1.0 - Taking Panorama Screenshots now will now be outputted into a resource pack format.
    Signed-off-by: Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>

--- src/main/java/com/mclegoman/perspective/client/panorama/PerspectivePanorama.java
+++ src/main/java/com/mclegoman/perspective/client/panorama/PerspectivePanorama.java
@@ -15,11 +15,15 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.Framebuffer;
+import net.minecraft.client.gl.SimpleFramebuffer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,33 +36,85 @@ public class PerspectivePanorama {
         for (String mod : INCOMPATIBLE) if (FabricLoader.getInstance().isModLoaded(mod)) INCOMPATIBLE_MODS_FOUND.add(mod);
     }
     public static void tick(MinecraftClient client) {
-        if (PerspectiveKeybindings.TAKE_PANORAMA_SCREENSHOT.wasPressed()) {
-            if (INCOMPATIBLE_MODS_FOUND.size() == 0) {
-                String time = getTime();
-                File directory = new File(client.runDirectory.getPath() + "/panorama/" + time);
-                if (directory.mkdirs()) {
-                    float YAW = 0;
-                    float PITCH = 0;
-                    if (client.player != null) {
-                        YAW = client.player.getYaw();
-                        PITCH = client.player.getPitch();
-                        client.player.setYaw(0);
-                        client.player.setPitch(0);
-                    }
-                    client.takePanorama(directory, 1024, 1024);
-                    if (client.player != null) {
-                        client.player.setYaw(YAW);
-                        client.player.setPitch(PITCH);
-                    }
-                }
-                sendMessage(PerspectiveTranslation.getTranslation("message.take_panorama_screenshot.success", new Object[]{Text.literal(time).formatted(Formatting.UNDERLINE).styled((style) -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, directory.getAbsolutePath())))}));
-            } else sendMessage(PerspectiveTranslation.getTranslation("message.take_panorama_screenshot.failure", new Object[]{INCOMPATIBLE_MODS_FOUND.toString().replace("[", "").replace("]", "")}));
-        }
+        if (PerspectiveKeybindings.TAKE_PANORAMA_SCREENSHOT.wasPressed()) takePanorama(1024);
     }
     private static String getTime() {
         return String.valueOf(LocalDateTime.now()).replace(":", "");
     }
-    private static void sendMessage(Text message) {
-        PerspectiveClientData.CLIENT.inGameHud.getChatHud().addMessage(message);
+    private static void takePanorama(int resolution) {
+        if (PerspectiveClientData.CLIENT.player != null) {
+            try {
+                if (INCOMPATIBLE_MODS_FOUND.size() == 0) {
+                    String panoramaName = getTime();
+                    String rpDirLoc = PerspectiveClientData.CLIENT.runDirectory.getPath() + "/panorama/" + panoramaName;
+                    String assetsDirLoc = rpDirLoc + "/assets/minecraft/textures/gui/title/background";
+                    if (new File(assetsDirLoc).mkdirs()) {
+                        int framebufferWidth = PerspectiveClientData.CLIENT.getWindow().getFramebufferWidth();
+                        int framebufferHeight = PerspectiveClientData.CLIENT.getWindow().getFramebufferHeight();
+                        Framebuffer framebuffer = new SimpleFramebuffer(resolution, resolution, true, MinecraftClient.IS_SYSTEM_MAC);
+                        float pitch = PerspectiveClientData.CLIENT.player.getPitch();
+                        float yaw = PerspectiveClientData.CLIENT.player.getYaw();
+                        PerspectiveClientData.CLIENT.gameRenderer.setBlockOutlineEnabled(false);
+                        PerspectiveClientData.CLIENT.gameRenderer.setRenderingPanorama(true);
+                        PerspectiveClientData.CLIENT.getWindow().setFramebufferWidth(resolution);
+                        PerspectiveClientData.CLIENT.getWindow().setFramebufferHeight(resolution);
+                        for(int l = 0; l < 6; ++l) {
+                            switch (l) {
+                                case 0 -> {
+                                    PerspectiveClientData.CLIENT.player.setYaw(0.0F);
+                                    PerspectiveClientData.CLIENT.player.setPitch(0.0F);
+                                }
+                                case 1 -> {
+                                    PerspectiveClientData.CLIENT.player.setYaw(90.0F);
+                                    PerspectiveClientData.CLIENT.player.setPitch(0.0F);
+                                }
+                                case 2 -> {
+                                    PerspectiveClientData.CLIENT.player.setYaw(180.0F);
+                                    PerspectiveClientData.CLIENT.player.setPitch(0.0F);
+                                }
+                                case 3 -> {
+                                    PerspectiveClientData.CLIENT.player.setYaw(270.0F);
+                                    PerspectiveClientData.CLIENT.player.setPitch(0.0F);
+                                }
+                                case 4 -> {
+                                    PerspectiveClientData.CLIENT.player.setYaw(0.0F);
+                                    PerspectiveClientData.CLIENT.player.setPitch(-90.0F);
+                                }
+                                default -> {
+                                    PerspectiveClientData.CLIENT.player.setYaw(0.0F);
+                                    PerspectiveClientData.CLIENT.player.setPitch(90.0F);
+                                }
+                            }
+                            framebuffer.beginWrite(true);
+                            PerspectiveClientData.CLIENT.gameRenderer.renderWorld(1.0F, 0L, new MatrixStack());
+                            try {Thread.sleep(10L);} catch (InterruptedException ignored) {}
+                            PerspectiveScreenshotRecorder.saveScreenshot(new File(assetsDirLoc), "panorama_" + l + ".png", framebuffer, (message) -> {
+                            });
+                        }
+                        File pack_file = new File(rpDirLoc + "/pack.mcmeta");
+                        if (pack_file.createNewFile()) {
+                            FileWriter pack_writer = new FileWriter(pack_file);
+                            pack_writer.write("{\"pack\": {\"pack_format\": 15, \"description\": \"" + panoramaName + "\"}}");
+                            pack_writer.close();
+                        }
+                        PerspectiveClientData.CLIENT.player.sendMessage(PerspectiveTranslation.getTranslation("message.take_panorama_screenshot.success", new Object[]{Text.literal(panoramaName).formatted(Formatting.UNDERLINE).styled((style) -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, new File(rpDirLoc).getAbsolutePath())))}));
+                        PerspectiveClientData.CLIENT.player.setPitch(pitch);
+                        PerspectiveClientData.CLIENT.player.setYaw(yaw);
+                        PerspectiveClientData.CLIENT.gameRenderer.setBlockOutlineEnabled(true);
+                        PerspectiveClientData.CLIENT.getWindow().setFramebufferWidth(framebufferWidth);
+                        PerspectiveClientData.CLIENT.getWindow().setFramebufferHeight(framebufferHeight);
+                        framebuffer.delete();
+                        PerspectiveClientData.CLIENT.gameRenderer.setRenderingPanorama(false);
+                        PerspectiveClientData.CLIENT.getFramebuffer().beginWrite(true);
+                    }
+                } else {
+                    PerspectiveData.LOGGER.error(PerspectiveData.PREFIX + "An error occurred whilst trying to take a panorama: Incompatible Mods: " + INCOMPATIBLE_MODS_FOUND.toString().replace("[", "").replace("]", ""));
+                    PerspectiveClientData.CLIENT.player.sendMessage(PerspectiveTranslation.getTranslation("message.take_panorama_screenshot.failure", new Object[]{PerspectiveTranslation.getTranslation("message.take_panorama_screenshot.failure.incompatible_mods", new Object[]{INCOMPATIBLE_MODS_FOUND.toString().replace("[", "").replace("]", "")})}));
+                }
+            } catch (Exception error) {
+                PerspectiveData.LOGGER.error(PerspectiveData.PREFIX + "An error occurred whilst trying to take a panorama: " + error);
+                PerspectiveClientData.CLIENT.player.sendMessage(PerspectiveTranslation.getTranslation("message.take_panorama_screenshot.failure", new Object[]{error}));
+            }
+        }
     }
 }
