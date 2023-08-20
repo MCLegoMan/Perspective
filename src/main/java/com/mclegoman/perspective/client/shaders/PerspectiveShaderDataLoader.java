c733d44995e2b3559d25da93e2f3910c56f5bb1f
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-08-20 17:15:12 +1200

    1.1.1 - Release Candidate 2; Shaders will no longer save the config when using the config screen until the screen has been exited.
    Signed-off-by: Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>

--- src/main/java/com/mclegoman/perspective/client/shaders/PerspectiveShaderDataLoader.java
+++ src/main/java/com/mclegoman/perspective/client/shaders/PerspectiveShaderDataLoader.java
@@ -120,7 +120,7 @@ public class PerspectiveShaderDataLoader extends JsonDataLoader implements Ident
             prepared.forEach(this::layout$perspective);
             layout$souper_secret_settings(manager);
             PerspectiveConfigHelper.setConfig("super_secret_settings", Math.min((int)PerspectiveConfigHelper.getConfig("super_secret_settings"), REGISTRY.size() - 1));
-            PerspectiveShader.set(MinecraftClient.getInstance(), true, true);
+            PerspectiveShader.set(MinecraftClient.getInstance(), true, true, true);
         } catch (Exception error) {
             PerspectiveData.LOGGER.error(PerspectiveData.PREFIX + "Failed to apply shaders dataloader: {}", (Object)error);
         }
