4bbe412b1d67ea4af80fad9cb28d2f45f729ca45
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-11-10 21:30:25 +1300

    Renamed `pig_overlay` to `pig_outer_layer` to match vanilla, Added `minecraft:pig#outer` model, Added `Mossy` skeleton.

---
+++ src/main/java/com/mclegoman/perspective/client/textured_entity/TexturedEntityModels.java
@@ -0,0 +1,21 @@
+/*
+    Perspective
+    Contributor(s): MCLegoMan
+    Github: https://github.com/MCLegoMan/Perspective
+    License: GNU LGPLv3
+*/
+
+package com.mclegoman.perspective.client.textured_entity;
+
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.minecraft.client.model.Dilation;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
+import net.minecraft.client.render.entity.model.PigEntityModel;
+import net.minecraft.util.Identifier;
+
+public class TexturedEntityModels {
+	public static final EntityModelLayer PIG_OVERLAY = new EntityModelLayer(new Identifier("minecraft", "pig"), "outer");
+	public static void init() {
+		EntityModelLayerRegistry.registerModelLayer(PIG_OVERLAY, () -> PigEntityModel.getTexturedModelData(new Dilation(0.499F)));
+	}
+}
--- src/main/java/com/mclegoman/perspective/mixin/client/textured_entity/minecraft/pig/PigEntityRendererMixin.java
+++ src/main/java/com/mclegoman/perspective/mixin/client/textured_entity/minecraft/pig/PigEntityRendererMixin.java
@@ -8,12 +8,12 @@
 package com.mclegoman.perspective.mixin.client.textured_entity.minecraft.pig;
 
 import com.mclegoman.perspective.client.textured_entity.TexturedEntity;
+import com.mclegoman.perspective.client.textured_entity.TexturedEntityModels;
 import com.mclegoman.perspective.client.textured_entity.features.PigOverlayFeatureRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PigEntityRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
-import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.client.render.entity.model.PigEntityModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.PigEntity;
@@ -31,7 +31,7 @@ public abstract class PigEntityRendererMixin extends LivingEntityRenderer {
     }
     @Inject(method = "<init>(Lnet/minecraft/client/render/entity/EntityRendererFactory$Context;)V", at = @At("TAIL"))
     private void perspective$init(EntityRendererFactory.Context context, CallbackInfo ci) {
-        this.addFeature(new PigOverlayFeatureRenderer(this, new PigEntityModel(context.getPart(EntityModelLayers.PIG_SADDLE)), "minecraft:pig", new Identifier("textures/entity/pig/pig_overlay.png")));
+        this.addFeature(new PigOverlayFeatureRenderer(this, new PigEntityModel(context.getPart(TexturedEntityModels.PIG_OVERLAY)), "minecraft:pig", new Identifier("textures/entity/pig/test_outer_layer.png")));
     }
     @Inject(at = @At("RETURN"), method = "getTexture(Lnet/minecraft/entity/Entity;)Lnet/minecraft/util/Identifier;", cancellable = true)
     private void perspective$getTexture(Entity entity, CallbackInfoReturnable<Identifier> cir) {
