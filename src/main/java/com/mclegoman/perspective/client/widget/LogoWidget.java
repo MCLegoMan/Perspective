4a0470d622e78d7032dde6a778d85fe30303a297
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-11-30 15:44:38 +1300

    1.2.0-beta.1 - Update to 1.20.3-pre.4

--- src/main/java/com/mclegoman/perspective/client/widget/LogoWidget.java
+++ src/main/java/com/mclegoman/perspective/client/widget/LogoWidget.java
@@ -14,19 +14,21 @@ import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
 
 public class LogoWidget extends ClickableWidget {
-
 	public LogoWidget() {
 		super(0, 0, 256, 48, Text.empty());
 	}
-
 	public void renderButton(DrawContext context, int mouseX, int mouseY, float delta) {
+		renderPerspectiveLogo(context, mouseX, mouseY, delta);
+	}
+	protected void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
+		renderPerspectiveLogo(context, mouseX, mouseY, delta);
+	}
+	private void renderPerspectiveLogo(DrawContext context, int mouseX, int mouseY, float delta) {
 		context.drawTexture(ClientData.getLogo(), this.getX(), this.getY(), 0.0F, 0.0F, this.getWidth(), this.getHeight(), this.getWidth(), this.getHeight());
 	}
-
 	@Override
 	protected void appendClickableNarrations(NarrationMessageBuilder builder) {
 	}
-
 	@Override
 	protected boolean isValidClickButton(int button) {
 		return false;
