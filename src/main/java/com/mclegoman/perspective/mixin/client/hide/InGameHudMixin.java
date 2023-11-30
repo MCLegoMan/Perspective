1513e9c35f12f4b021e35a3bd12633cd8037b00d
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-12-01 04:27:47 +1300

    1.2.0-beta.1 - Fix crosshair on hide_crosshair dynamic/false modes.

--- src/main/java/com/mclegoman/perspective/mixin/client/hide/InGameHudMixin.java
+++ src/main/java/com/mclegoman/perspective/mixin/client/hide/InGameHudMixin.java
@@ -16,13 +16,17 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(priority = 10000, value = InGameHud.class)
 public abstract class InGameHudMixin {
+    @Shadow @Final private static Identifier ICONS;
+
     @Inject(at = @At("HEAD"), method = "renderCrosshair", cancellable = true)
     private void perspective$renderCrosshair(DrawContext context, CallbackInfo ci) {
         if (ClientData.CLIENT.world != null) {
@@ -32,25 +36,27 @@ public abstract class InGameHudMixin {
                 if (crosshairTarget != null) {
                     if ((ConfigHelper.getConfig("hide_crosshair").equals("dynamic"))) hide_crosshair = (crosshairTarget.getType() == HitResult.Type.BLOCK) ? ClientData.CLIENT.world.getBlockState(((BlockHitResult)crosshairTarget).getBlockPos()).isAir() : crosshairTarget.getType() != HitResult.Type.ENTITY;
                     if (hide_crosshair) {
-                        if (ClientData.CLIENT.player != null) {
-                            if (ClientData.CLIENT.options.getAttackIndicator().getValue() == AttackIndicator.CROSSHAIR) {
-                                float cooldownProgress = ClientData.CLIENT.player.getAttackCooldownProgress(0.0F);
-                                boolean cooldownProgressFull = false;
-                                if (ClientData.CLIENT.targetedEntity instanceof LivingEntity && cooldownProgress >= 1.0F) {
-                                    cooldownProgressFull = ClientData.CLIENT.player.getAttackCooldownProgressPerTick() > 5.0F;
-                                    cooldownProgressFull &= ClientData.CLIENT.targetedEntity.isAlive();
+                        if (ClientData.CLIENT.options.getAttackIndicator().getValue() == AttackIndicator.CROSSHAIR) {
+                            if (ClientData.CLIENT.player != null) {
+                                float attackCooldownProgress = ClientData.CLIENT.player.getAttackCooldownProgress(0.0F);
+                                boolean bl = false;
+                                if (ClientData.CLIENT.targetedEntity instanceof LivingEntity && attackCooldownProgress >= 1.0F) {
+                                    bl = ClientData.CLIENT.player.getAttackCooldownProgressPerTick() > 5.0F;
+                                    bl &= ClientData.CLIENT.targetedEntity.isAlive();
                                 }
+
                                 int x = ClientData.CLIENT.getWindow().getScaledWidth() / 2 - 8;
                                 int y = ClientData.CLIENT.getWindow().getScaledHeight() / 2 - 7 + 16;
-                                if (cooldownProgressFull) {
-                                    context.drawTexture(new Identifier("hud/crosshair_attack_indicator_full"), x, y, 0, 0, 16, 16);
-                                } else if (cooldownProgress < 1.0F) {
-                                    context.drawTexture(new Identifier("hud/crosshair_attack_indicator_background"), x, y, 0, 0, 16, 4);
-                                    context.drawTexture(new Identifier("hud/crosshair_attack_indicator_progress"), 16, 4, 0, 0, x, y, (int) (cooldownProgress * 17.0F), 4);
+                                if (bl) {
+                                    context.drawTexture(ICONS, x, y, 68, 94, 16, 16);
+                                } else if (attackCooldownProgress < 1.0F) {
+                                    int progressWidth = (int)(attackCooldownProgress * 17.0F);
+                                    context.drawTexture(ICONS, x, y, 36, 94, 16, 4);
+                                    context.drawTexture(ICONS, x, y, 52, 94, progressWidth, 4);
                                 }
                             }
-                            ci.cancel();
                         }
+                        ci.cancel();
                     }
                 }
             }
