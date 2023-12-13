9b45e72e3d3e6bed850509121add716e0ad61394
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-12-14 10:47:23 +1300

    1.x.x - Added eyes textured entity texture compatibility for enderman, spider, and phantom.

---
+++ src/main/java/com/mclegoman/perspective/mixin/client/textured_entity/minecraft/enderman/EyesFeatureRendererMixin.java
@@ -0,0 +1,50 @@
+/*
+    Perspective
+    Contributor(s): MCLegoMan
+    Github: https://github.com/MCLegoMan/Perspective
+    Licence: GNU LGPLv3
+*/
+
+package com.mclegoman.perspective.mixin.client.textured_entity.minecraft.enderman;
+
+import com.mclegoman.perspective.client.textured_entity.TexturedEntity;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.EyesFeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRendererContext;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.entity.mob.PhantomEntity;
+import net.minecraft.entity.mob.SpiderEntity;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(priority = 10000, value = EyesFeatureRenderer.class)
+public class EyesFeatureRendererMixin <T extends Entity, M extends EntityModel<T>> extends FeatureRenderer<T, M> {
+	public EyesFeatureRendererMixin(FeatureRendererContext<T, M> context) {
+		super(context);
+	}
+	@Override
+	public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, T entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
+		if (entity instanceof EndermanEntity) {
+			RenderLayer eyesTexture = RenderLayer.getEyes(TexturedEntity.getTexture(entity, "minecraft:enderman", "_eyes", new Identifier("textures/entity/enderman/enderman_eyes.png")));
+			VertexConsumer vertexConsumer = vertexConsumers.getBuffer(eyesTexture);
+			this.getContextModel().render(matrices, vertexConsumer, 15728640, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
+		}
+		else if (entity instanceof SpiderEntity) {
+			RenderLayer eyesTexture = RenderLayer.getEyes(TexturedEntity.getTexture(entity, "minecraft:spider", "_eyes", new Identifier("textures/entity/spider_eyes.png")));
+			VertexConsumer vertexConsumer = vertexConsumers.getBuffer(eyesTexture);
+			this.getContextModel().render(matrices, vertexConsumer, 15728640, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
+		}
+		else if (entity instanceof PhantomEntity) {
+			RenderLayer eyesTexture = RenderLayer.getEyes(TexturedEntity.getTexture(entity, "minecraft:phantom", "_eyes", new Identifier("textures/entity/phantom_eyes.png")));
+			VertexConsumer vertexConsumer = vertexConsumers.getBuffer(eyesTexture);
+			this.getContextModel().render(matrices, vertexConsumer, 15728640, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
+		}
+	}
+}
