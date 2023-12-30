e98bfd1431abf339b81e07b79c30c935360650d9
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-12-30 15:46:22 +1300

    1.3.0-alpha.3 - Shaders will no longer be registered if `shaders/post/<name>.json` cannot be found.

--- src/main/java/com/mclegoman/perspective/client/shaders/ShaderDataLoader.java
+++ src/main/java/com/mclegoman/perspective/client/shaders/ShaderDataLoader.java
@@ -13,14 +13,17 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.mclegoman.perspective.client.config.ConfigHelper;
 import com.mclegoman.perspective.common.data.Data;
+import com.mclegoman.releasetypeutils.common.version.Helper;
 import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.minecraft.resource.JsonDataLoader;
 import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceFactory;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
 import net.minecraft.util.profiler.Profiler;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -51,68 +54,73 @@ public class ShaderDataLoader extends JsonDataLoader implements IdentifiableReso
 		return null;
 	}
 
-	private void add(String NAMESPACE, String SHADER_NAME, boolean DISABLE_SCREEN_MODE, boolean ENABLED) {
+	private void add(String NAMESPACE, String SHADER_NAME, boolean DISABLE_SCREEN_MODE, boolean ENABLED, ResourceManager manager) {
 		try {
 			SHADER_NAME = SHADER_NAME.replace("\"", "").toLowerCase();
 			Identifier ID = new Identifier(NAMESPACE.toLowerCase(), ("shaders/post/" + SHADER_NAME + ".json"));
-			List<Object> SHADER_MAP = new ArrayList<>();
-			SHADER_MAP.add(ID);
-			SHADER_MAP.add(NAMESPACE.toLowerCase());
-			SHADER_MAP.add(SHADER_NAME);
-			SHADER_MAP.add(DISABLE_SCREEN_MODE);
-			boolean ALREADY_REGISTERED = false;
-			List<Object> REGISTRY_MAP = SHADER_MAP;
-			for (List<Object> SHADER_MAP_IN_REGISTRY : REGISTRY) {
-				if (SHADER_MAP_IN_REGISTRY.contains(ID)) {
-					ALREADY_REGISTERED = true;
-					REGISTRY_MAP = SHADER_MAP_IN_REGISTRY;
-					break;
+			try {
+				manager.getResourceOrThrow(ID);
+				List<Object> SHADER_MAP = new ArrayList<>();
+				SHADER_MAP.add(ID);
+				SHADER_MAP.add(NAMESPACE.toLowerCase());
+				SHADER_MAP.add(SHADER_NAME);
+				SHADER_MAP.add(DISABLE_SCREEN_MODE);
+				boolean ALREADY_REGISTERED = false;
+				List<Object> REGISTRY_MAP = SHADER_MAP;
+				for (List<Object> SHADER_MAP_IN_REGISTRY : REGISTRY) {
+					if (SHADER_MAP_IN_REGISTRY.contains(ID)) {
+						ALREADY_REGISTERED = true;
+						REGISTRY_MAP = SHADER_MAP_IN_REGISTRY;
+						break;
+					}
 				}
+				if (ENABLED) {
+					if (!ALREADY_REGISTERED) REGISTRY.add(SHADER_MAP);
+				} else REGISTRY.remove(REGISTRY_MAP);
+			} catch (FileNotFoundException error) {
+				Data.PERSPECTIVE_VERSION.sendToLog(Helper.LogType.WARN, "Failed to register shader: " + error);
 			}
-			if (ENABLED) {
-				if (!ALREADY_REGISTERED) REGISTRY.add(SHADER_MAP);
-			} else REGISTRY.remove(REGISTRY_MAP);
 		} catch (Exception error) {
-			Data.PERSPECTIVE_VERSION.getLogger().warn("{} Failed to add shader to registry: {}", Data.PERSPECTIVE_VERSION.getID(), error);
+			Data.PERSPECTIVE_VERSION.sendToLog(Helper.LogType.WARN, "Failed to add shader to registry: " + error);
 		}
 	}
 
-	private void reset() {
+	private void reset(ResourceManager manager) {
 		try {
 			REGISTRY.clear();
 			DUPLICATED_NAMES.clear();
-			add$default();
+			add$default(manager);
 		} catch (Exception error) {
 			Data.PERSPECTIVE_VERSION.getLogger().warn("{} Failed to reset shaders registry: {}", Data.PERSPECTIVE_VERSION.getID(), error);
 		}
 	}
 
-	private void add$default() {
+	private void add$default(ResourceManager manager) {
 		try {
-			add("minecraft", "antialias", false, true);
-			add("minecraft", "art", true, true);
-			add("minecraft", "bits", true, true);
-			add("minecraft", "blobs", true, true);
-			add("minecraft", "blobs2", true, true);
-			add("minecraft", "blur", true, true);
-			add("minecraft", "bumpy", false, true);
-			add("minecraft", "color_convolve", false, true);
-			add("minecraft", "creeper", true, true);
-			add("minecraft", "deconverge", false, true);
-			add("minecraft", "desaturate", false, true);
-			add("minecraft", "flip", true, true);
-			add("minecraft", "fxaa", false, true);
-			add("minecraft", "green", true, true);
-			add("minecraft", "invert", false, true);
-			add("minecraft", "notch", false, true);
-			add("minecraft", "ntsc", true, true);
-			add("minecraft", "outline", false, true);
-			add("minecraft", "pencil", true, true);
-			add("minecraft", "phosphor", false, true);
-			add("minecraft", "scan_pincushion", false, true);
-			add("minecraft", "sobel", false, true);
-			add("minecraft", "spider", true, true);
-			add("minecraft", "wobble", false, true);
+			add("minecraft", "antialias", false, true, manager);
+			add("minecraft", "art", true, true, manager);
+			add("minecraft", "bits", true, true, manager);
+			add("minecraft", "blobs", true, true, manager);
+			add("minecraft", "blobs2", true, true, manager);
+			add("minecraft", "blur", true, true, manager);
+			add("minecraft", "bumpy", false, true, manager);
+			add("minecraft", "color_convolve", false, true, manager);
+			add("minecraft", "creeper", true, true, manager);
+			add("minecraft", "deconverge", false, true, manager);
+			add("minecraft", "desaturate", false, true, manager);
+			add("minecraft", "flip", true, true, manager);
+			add("minecraft", "fxaa", false, true, manager);
+			add("minecraft", "green", true, true, manager);
+			add("minecraft", "invert", false, true, manager);
+			add("minecraft", "notch", false, true, manager);
+			add("minecraft", "ntsc", true, true, manager);
+			add("minecraft", "outline", false, true, manager);
+			add("minecraft", "pencil", true, true, manager);
+			add("minecraft", "phosphor", false, true, manager);
+			add("minecraft", "scan_pincushion", false, true, manager);
+			add("minecraft", "sobel", false, true, manager);
+			add("minecraft", "spider", true, true, manager);
+			add("minecraft", "wobble", false, true, manager);
 		} catch (Exception error) {
 			Data.PERSPECTIVE_VERSION.getLogger().warn("{} Failed to add default shaders to registry: {}", Data.PERSPECTIVE_VERSION.getID(), error);
 		}
@@ -121,8 +129,8 @@ public class ShaderDataLoader extends JsonDataLoader implements IdentifiableReso
 	@Override
 	public void apply(Map<Identifier, JsonElement> prepared, ResourceManager manager, Profiler profiler) {
 		try {
-			reset();
-			prepared.forEach(this::layout$perspective);
+			reset(manager);
+			prepared.forEach((identifier, jsonElement) -> layout$perspective(identifier, jsonElement, manager));
 			layout$souper_secret_settings(manager);
 			boolean saveConfig;
 			if (Shader.updateLegacyConfig) {
@@ -158,14 +166,14 @@ public class ShaderDataLoader extends JsonDataLoader implements IdentifiableReso
 		return new Identifier(Data.PERSPECTIVE_VERSION.getID(), ID);
 	}
 
-	private void layout$perspective(Identifier identifier, JsonElement jsonElement) {
+	private void layout$perspective(Identifier identifier, JsonElement jsonElement, ResourceManager manager) {
 		try {
 			JsonObject READER = jsonElement.getAsJsonObject();
 			String NAMESPACE = JsonHelper.getString(READER, "namespace", Data.PERSPECTIVE_VERSION.getID());
 			String SHADER = JsonHelper.getString(READER, "shader");
 			boolean DISABLE_SCREEN_MODE = JsonHelper.getBoolean(READER, "disable_screen_mode", false);
 			boolean ENABLED = JsonHelper.getBoolean(READER, "enabled", true);
-			add(NAMESPACE, SHADER, DISABLE_SCREEN_MODE, ENABLED);
+			add(NAMESPACE, SHADER, DISABLE_SCREEN_MODE, ENABLED, manager);
 		} catch (Exception error) {
 			Data.PERSPECTIVE_VERSION.getLogger().warn("{} Failed to load perspective shader: {}", Data.PERSPECTIVE_VERSION.getID(), error);
 		}
@@ -181,7 +189,7 @@ public class ShaderDataLoader extends JsonDataLoader implements IdentifiableReso
 					for (JsonElement SHADER : JsonHelper.getArray(NAMESPACES, "disable_screen_mode", new JsonArray()))
 						DISABLE_SCREEN_MODE_SHADERS.add(SHADER.getAsString());
 					for (JsonElement SHADER : JsonHelper.getArray(NAMESPACES, "shaders", new JsonArray()))
-						add(JsonHelper.getString(NAMESPACES, "namespace", Data.PERSPECTIVE_VERSION.getID()), SHADER.getAsString(), DISABLE_SCREEN_MODE_SHADERS.contains(SHADER.getAsString()), JsonHelper.getBoolean(NAMESPACES, "enabled", true));
+						add(JsonHelper.getString(NAMESPACES, "namespace", Data.PERSPECTIVE_VERSION.getID()), SHADER.getAsString(), DISABLE_SCREEN_MODE_SHADERS.contains(SHADER.getAsString()), JsonHelper.getBoolean(NAMESPACES, "enabled", true), manager);
 				}
 			} catch (Exception error) {
 				Data.PERSPECTIVE_VERSION.getLogger().warn("{} Failed to load souper secret settings shader list: {}", Data.PERSPECTIVE_VERSION.getID(), error);
