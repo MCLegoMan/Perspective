3438fdb16435452cf86272f3e9fb3db8008aec5a
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-01-26 17:39:45 +1300

    1.3.0-alpha.4 - Fixed DisplayNames chat message override.

--- src/main/java/com/mclegoman/perspective/mixin/client/displaynames/TranslatableTextContentMixin.java
+++ src/main/java/com/mclegoman/perspective/mixin/client/displaynames/TranslatableTextContentMixin.java
@@ -14,10 +14,11 @@ import com.mclegoman.perspective.common.util.Couple;
 import com.mclegoman.releasetypeutils.common.version.Helper;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.scoreboard.Team;
-import net.minecraft.text.StringVisitable;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableTextContent;
+import net.minecraft.text.*;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -26,6 +27,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 @Mixin(priority = 10000, value = TranslatableTextContent.class)
@@ -42,13 +45,22 @@ public abstract class TranslatableTextContentMixin {
 						if (this.key.startsWith("chat.type")) {
 							if (index != (this.args.length - 1)) {
 								if (ClientData.CLIENT.world != null) {
-									List<AbstractClientPlayerEntity> players = ClientData.CLIENT.world.getPlayers();
-									if (!players.isEmpty()) {
-										for (AbstractClientPlayerEntity playerEntity : players) {
-											Data.VERSION.sendToLog(Helper.LogType.INFO, playerEntity.getUuid().toString());
-											for (Couple<UUID, Text> player : DisplayNamesDataLoader.REGISTRY) {
-												if (player.getFirst().equals(playerEntity.getUuid())) {
-													cir.setReturnValue(Team.decorateName(playerEntity.getScoreboardTeam(), player.getSecond()).setStyle(((Text) cir.getReturnValue()).getStyle()));
+									HoverEvent hoverEvent = ((Text) cir.getReturnValue()).getStyle().getHoverEvent();
+									Data.VERSION.sendToLog(Helper.LogType.INFO, "1");
+									if (hoverEvent != null && hoverEvent.getValue(HoverEvent.Action.SHOW_ENTITY) != null) {
+										Data.VERSION.sendToLog(Helper.LogType.INFO, "2");
+										if (Objects.requireNonNull(hoverEvent.getValue(HoverEvent.Action.SHOW_ENTITY)).entityType == EntityType.PLAYER) {
+											Data.VERSION.sendToLog(Helper.LogType.INFO, "3");
+											List<AbstractClientPlayerEntity> players = ClientData.CLIENT.world.getPlayers();
+											UUID uuid = Objects.requireNonNull(hoverEvent.getValue(HoverEvent.Action.SHOW_ENTITY)).uuid;
+											if (!players.isEmpty()) {
+												for (AbstractClientPlayerEntity playerEntity : players) {
+													for (Couple<UUID, Text> player : DisplayNamesDataLoader.REGISTRY) {
+														if (uuid.equals(player.getFirst())) {
+															Data.VERSION.sendToLog(Helper.LogType.INFO, "4");
+															cir.setReturnValue(Team.decorateName(playerEntity.getScoreboardTeam(), player.getSecond()).setStyle(((Text) cir.getReturnValue()).getStyle()));
+														}
+													}
 												}
 											}
 										}
