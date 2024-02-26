22d51c33b56c7aa201ba5ae472b16652a28d40e1
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-02-26 22:38:57 +1300

    1.3.0-alpha.4 - Updated RenderPhaseMixin to use Redirect instead of Inject.

--- src/main/java/com/mclegoman/perspective/mixin/client/super_secret_settings/RenderPhaseMixin.java
+++ src/main/java/com/mclegoman/perspective/mixin/client/super_secret_settings/RenderPhaseMixin.java
@@ -10,91 +10,88 @@ package com.mclegoman.perspective.mixin.client.super_secret_settings;
 import com.mclegoman.perspective.client.shaders.Shader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderPhase;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = RenderPhase.class, priority = 100)
 public abstract class RenderPhaseMixin {
-	@Mutable
-	@Shadow @Final public static RenderPhase.Target TRANSLUCENT_TARGET;
-	@Mutable
-	@Shadow @Final public static RenderPhase.Target ITEM_ENTITY_TARGET;
-
-	@Mutable
-	@Shadow @Final public static RenderPhase.Target PARTICLES_TARGET;
-
-	@Mutable
-	@Shadow @Final public static RenderPhase.Target WEATHER_TARGET;
-
-	@Mutable
-	@Shadow @Final public static RenderPhase.Target CLOUDS_TARGET;
-
-	@Inject(method = "<clinit>", at = @At("RETURN"))
-	private static void perspective$overrideTargets(CallbackInfo ci) {
-		TRANSLUCENT_TARGET = new RenderPhase.Target("translucent_target", () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter()) {
-				MinecraftClient.getInstance().worldRenderer.getTranslucentFramebuffer().beginWrite(false);
+	@Redirect(method = "<clinit>", at = @At(value = "NEW", target = "(Ljava/lang/String;Ljava/lang/Runnable;Ljava/lang/Runnable;)Lnet/minecraft/client/render/RenderPhase$Target;", ordinal = 2))
+	private static RenderPhase.Target perspective$overrideTargets(String string, Runnable runnable, Runnable runnable2) {
+		switch (string) {
+			case ("translucent_target") -> {
+				return new RenderPhase.Target("translucent_target", () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter()) {
+						MinecraftClient.getInstance().worldRenderer.getTranslucentFramebuffer().beginWrite(false);
+					}
+					if (Shader.shouldRenderShader()) {
+						if (Shader.translucentFramebuffer != null) Shader.translucentFramebuffer.beginWrite(false);
+					}
+				}, () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
+						MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
+					}
+				});
 			}
-			if (Shader.shouldRenderShader()) {
-				if (Shader.translucentFramebuffer != null) Shader.translucentFramebuffer.beginWrite(false);
+			case ("item_entity_target") -> {
+				return new RenderPhase.Target("item_entity_target", () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter()) {
+						MinecraftClient.getInstance().worldRenderer.getEntityFramebuffer().beginWrite(false);
+					}
+					if (Shader.shouldRenderShader()) {
+						if (Shader.entityFramebuffer != null) Shader.entityFramebuffer.beginWrite(false);
+					}
+				}, () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
+						MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
+					}
+				});
 			}
-		}, () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
-				MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
+			case ("particles_target") -> {
+				return new RenderPhase.Target("particles_target", () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter()) {
+						MinecraftClient.getInstance().worldRenderer.getParticlesFramebuffer().beginWrite(false);
+					}
+					if (Shader.shouldRenderShader()) {
+						if (Shader.particlesFramebuffer != null) Shader.particlesFramebuffer.beginWrite(false);
+					}
+				}, () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
+						MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
+					}
+				});
 			}
-		});
-		ITEM_ENTITY_TARGET = new RenderPhase.Target("item_entity_target", () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter()) {
-				MinecraftClient.getInstance().worldRenderer.getEntityFramebuffer().beginWrite(false);
+			case ("weather_target") -> {
+				return new RenderPhase.Target("weather_target", () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter()) {
+						MinecraftClient.getInstance().worldRenderer.getWeatherFramebuffer().beginWrite(false);
+					}
+					if (Shader.shouldRenderShader()) {
+						if (Shader.weatherFramebuffer != null) Shader.weatherFramebuffer.beginWrite(false);
+					}
+				}, () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
+						MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
+					}
+				});
 			}
-			if (Shader.shouldRenderShader()) {
-				if (Shader.entityFramebuffer != null) Shader.entityFramebuffer.beginWrite(false);
+			case ("clouds_target") -> {
+				return new RenderPhase.Target("clouds_target", () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter()) {
+						MinecraftClient.getInstance().worldRenderer.getCloudsFramebuffer().beginWrite(false);
+					}
+					if (Shader.shouldRenderShader()) {
+						if (Shader.cloudsFramebuffer != null) Shader.cloudsFramebuffer.beginWrite(false);
+					}
+				}, () -> {
+					if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
+						MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
+					}
+				});
 			}
-		}, () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
-				MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
+			default -> {
+				return new RenderPhase.Target(string, runnable, runnable2);
 			}
-		});
-		PARTICLES_TARGET = new RenderPhase.Target("particles_target", () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter()) {
-				MinecraftClient.getInstance().worldRenderer.getParticlesFramebuffer().beginWrite(false);
-			}
-			if (Shader.shouldRenderShader()) {
-				if (Shader.particlesFramebuffer != null) Shader.particlesFramebuffer.beginWrite(false);
-			}
-		}, () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
-				MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
-			}
-		});
-		WEATHER_TARGET = new RenderPhase.Target("weather_target", () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter()) {
-				MinecraftClient.getInstance().worldRenderer.getWeatherFramebuffer().beginWrite(false);
-			}
-			if (Shader.shouldRenderShader()) {
-				if (Shader.weatherFramebuffer != null) Shader.weatherFramebuffer.beginWrite(false);
-			}
-		}, () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
-				MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
-			}
-		});
-		CLOUDS_TARGET = new RenderPhase.Target("clouds_target", () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter()) {
-				MinecraftClient.getInstance().worldRenderer.getCloudsFramebuffer().beginWrite(false);
-			}
-			if (Shader.shouldRenderShader()) {
-				if (Shader.cloudsFramebuffer != null) Shader.cloudsFramebuffer.beginWrite(false);
-			}
-		}, () -> {
-			if (MinecraftClient.isFabulousGraphicsOrBetter() || Shader.shouldRenderShader()) {
-				MinecraftClient.getInstance().getFramebuffer().beginWrite(false);
-			}
-		});
+		}
 	}
 }
