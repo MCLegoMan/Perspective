5ccc598aa856710865bef0fabf86ebbf2f5a2073
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-05-08 17:24:16 +1200

    1.3.0-alpha.7 - Ported CEM Compatibility from 1.2.2.

--- src/main/java/com/mclegoman/perspective/common/data/Data.java
+++ src/main/java/com/mclegoman/perspective/common/data/Data.java
@@ -11,5 +11,5 @@ import com.mclegoman.luminance.common.data.Version;
 import com.mclegoman.releasetypeutils.common.version.Helper;
 
 public class Data extends com.mclegoman.luminance.common.data.Data {
-	public static final Version version = Version.create("Perspective", "perspective", 1, 3, 0, Helper.ReleaseType.ALPHA, 6, "6CTGnrNg");
+	public static final Version version = Version.create("Perspective", "perspective", 1, 3, 0, Helper.ReleaseType.ALPHA, 7, "6CTGnrNg");
 }
