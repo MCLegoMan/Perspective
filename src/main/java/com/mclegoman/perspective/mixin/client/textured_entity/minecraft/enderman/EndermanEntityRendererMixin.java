95f84ae1f6d0c26eaef7bef7d86c7bd53ea1c3a7
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-08-19 21:55:20 +1200

    1.3.0-alpha.8 - Added capes to endermen.

--- src/main/java/com/mclegoman/perspective/mixin/client/textured_entity/minecraft/enderman/EndermanEntityRendererMixin.java
+++ src/main/java/com/mclegoman/perspective/mixin/client/textured_entity/minecraft/enderman/EndermanEntityRendererMixin.java
@@ -7,16 +7,30 @@
 
 package com.mclegoman.perspective.mixin.client.textured_entity.minecraft.enderman;
 
+import com.mclegoman.perspective.client.entity.EntityModels;
 import com.mclegoman.perspective.client.entity.TexturedEntity;
+import com.mclegoman.perspective.client.entity.model.LivingEntityCapeModel;
+import com.mclegoman.perspective.client.entity.renderer.feature.EntityCapeFeatureRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.EndermanEntityModel;
 import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(priority = 100, value = net.minecraft.client.render.entity.EndermanEntityRenderer.class)
-public class EndermanEntityRendererMixin {
+public abstract class EndermanEntityRendererMixin extends MobEntityRenderer<EndermanEntity, EndermanEntityModel<EndermanEntity>> {
+	public EndermanEntityRendererMixin(EntityRendererFactory.Context context, EndermanEntityModel<EndermanEntity> entityModel, float f) {
+		super(context, entityModel, f);
+	}
+	@Inject(method = "<init>(Lnet/minecraft/client/render/entity/EntityRendererFactory$Context;)V", at = @At("TAIL"))
+	private void perspective$init(EntityRendererFactory.Context context, CallbackInfo ci) {
+		this.addFeature(new EntityCapeFeatureRenderer.Builder(this, new LivingEntityCapeModel(context.getPart(EntityModels.entityCape)), Identifier.of("perspective", "textures/entity/minecraft/enderman/enderman_cape.png")).offsetY(-0.8125F).build());
+	}
 	@Inject(at = @At("RETURN"), method = "getTexture(Lnet/minecraft/entity/mob/EndermanEntity;)Lnet/minecraft/util/Identifier;", cancellable = true)
 	private void perspective$getTexture(EndermanEntity entity, CallbackInfoReturnable<Identifier> cir) {
 		cir.setReturnValue(TexturedEntity.getTexture(entity, cir.getReturnValue()));
